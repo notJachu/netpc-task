7f2183ed8f068d50f98dfba4a9e4e0906cc7faf9
notJachu <jasiustasiu50@interia.pl>
2026-06-07 18:48:22 +0200

    init contact contoller

---
+++ backend/Controllers/ContactsController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ContactsController : ControllerBase
+{
+
+    [HttpGet]
+    [Route("list")]
+    public IActionResult GetContactList()
+    {
+        
+        return Ok();
+    }
+    
+    [HttpGet]
+    [Route("{id}")]
+    public IActionResult GetContact(int id)
+    {
+        return Ok();
+    }
+
+    [HttpPost]
+    [Route("add")]
+    public IActionResult AddContact()
+    {
+        
+        return Ok();
+    }
+
+    [HttpPut]
+    [Route("{id}")]
+    public IActionResult UpdateContact(int id)
+    {
+        return Ok();
+    }
+
+    [HttpDelete]
+    [Route("{id}")]
+    public IActionResult DeleteContact(int id)
+    {
+        return Ok();
+    }
+
+}
