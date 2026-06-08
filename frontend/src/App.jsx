471c315cfbfe24cd030e165613c5a50d1bc39ce0
notJachu <jasiustasiu50@interia.pl>
2026-06-08 19:04:33 +0200

    refactor navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Link, Route, Routes, useNavigate} from "react-router-dom";
 import ProtectedRoute from "./components/ProtectedRoute.jsx";
 import Home from './pages/Home.jsx';
 import Contacts from './pages/Contacts.jsx';
@@ -7,31 +7,51 @@ import Login from './pages/Login.jsx';
 import ContactDetails from './pages/ContactDetails.jsx';
 import NotFound from './pages/NotFound.jsx';
 
+function Navigation() {
+    const isLoggedIn = !!localStorage.getItem('isLoggedIn');
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        localStorage.removeItem('isLoggedIn');
+        navigate('/login');
+    };
+
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-3">
+          <Link to="/" className="navbar-brand">DEMO APP</Link>
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav me-auto">
+                <li className="nav-item">
+                    <Link to="/contacts" className="nav-link">Contacts</Link>
+                </li>
+            </ul>
+            <ul className="navbar-nav">
+                {isLoggedIn ? (
+                    <li className="nav-item">
+                        <button onClick={handleLogout} className="btn btn-link nav-link">Log out</button>
+                    </li>
+                ) : (
+                    <li className="nav-item">
+                        <Link to="/login" className="nav-link">Log in</Link>
+                    </li>
+                )}
+            </ul>
+          </div>
+        </nav>
+    );
+}
+
 function App() {
 
   return (
     <>
       <BrowserRouter>
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-3">
-          <Link to="/" className="navbar-brand">Contacts App</Link>
-          <div className="collapse navbar-collapse">
-            <ul className="navbar-nav">
-                <li className="nav-item">
-                    <Link to="/contacts" className="nav-link">Contacts</Link>
-                </li>
-                <li className="nav-item">
-                    <Link to="/login" className="nav-link">Login</Link>
-                </li>
-            </ul>
-          </div>
-        </nav>
-        
+          <Navigation />
         <div className="container mt-4">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/contacts" element={<Contacts />} />
             <Route path="/login" element={<Login />} />
-            
             <Route
               path="/contacts/:id"
               element={
